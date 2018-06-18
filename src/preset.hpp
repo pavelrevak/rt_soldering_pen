e972c1e0d5d895e0c12a706077ef9c344c480826
Pavel Revak <pavel.revak@gmail.com>
2018-06-18 21:58:35 +0200

    cleaned preset module

--- src/preset.hpp
+++ src/preset.hpp
@@ -13,70 +13,130 @@ class Preset {
     static const int PRESET_TEMPERATURE_MAX = 400 * 1000;  // 400 degree C
     static const int STANDBY_TEMPERATURE = 0;  //  20 degree C
 
-    int temperatures[PRESETS] = {
+    int _temperatures[PRESETS] = {
         300 * 1000,
         250 * 1000,
     };
-    int selected = 0;
-    int edited = NO_EDIT;
-    bool standby = true;
+    int _selected = 0;
+    int _edited = NO_EDIT;
+    bool _standby = true;
 
 public:
-    inline void set_standby() {
-        standby = true;
+    /** Enter standby mode
+    */
+    void set_standby() {
+        _standby = true;
     }
 
-    inline bool is_standby() {
-        return standby;
+    /** check if is in standby mode
+
+    Return:
+        true if is in standby mode
+    */
+    bool is_standby() {
+        return _standby;
     }
 
-    inline void select(int preset) {
+    /** Select preset number of temperature
+
+    Arguments:
+        preset: preset number to enable
+    */
+    void select(int preset) {
         if ((preset < 0) && (preset >= PRESETS)) return;
-        selected = preset;
-        standby = false;
+        _selected = preset;
+        _standby = false;
     }
 
-    inline void edit_select(int preset) {
+    /** Select preset number of temperature for editing
+
+    Arguments:
+        preset: preset number to edit
+    */
+    void edit_select(int preset) {
         if ((preset < 0) && (preset >= PRESETS)) return;
-        edited = preset;
+        _edited = preset;
     }
 
-    inline void edit_end() {
-        edited = NO_EDIT;
+    /** Stop editing
+    */
+    void edit_end() {
+        _edited = NO_EDIT;
     }
 
-    inline int get_temperature() {
-        if (standby) return STANDBY_TEMPERATURE;
-        return temperatures[selected];
+    /** Read selected temperature
+
+    Return:
+        selected temperature or if is in standby it return default standby temperature
+    */
+    int get_temperature() {
+        if (_standby) return STANDBY_TEMPERATURE;
+        return _temperatures[_selected];
     }
 
-    inline int get_preset(int preset) {
-        return temperatures[preset];
+    /** Read preset temperature
+
+    Arguments:
+        preset: preset temperature to edit
+
+    Return:
+        preset temperature
+    */
+    int get_preset(int preset) {
+        return _temperatures[preset];
     }
 
-    inline int get_selected() {
-        return selected;
+    /** Read selected preset
+
+    Return:
+        selected preset
+    */
+    int get_selected() {
+        return _selected;
     }
 
-    inline int get_edited() {
-        return edited;
+    /** Read edited preset
+
+    Return:
+        edited preset
+    */
+    int get_edited() {
+        return _edited;
     }
 
-    inline bool is_editing() {
-        return edited != NO_EDIT;
+    /** Read if is editing
+
+    Return:
+        true if is editing
+    */
+    bool is_editing() {
+        return _edited != NO_EDIT;
     }
 
-    inline bool is_editing(int preset) {
-        return edited == preset;
+    /** Read if preset is editing
+
+    Arguments:
+        preset: preset
+
+    Return:
+        true if preset is editing
+    */
+    bool is_editing(int preset) {
+        return _edited == preset;
     }
 
+    /** Add to edited value
+
+    Arguments:
+        val: value to add (positive) subtract (negative) from edited temperature
+    */
     void edit_add(int val) {
-        if (edited == NO_EDIT) return;
-        temperatures[edited] += val;
-        if (temperatures[edited] < MIN_TEMPERATURE) {
-            temperatures[edited] = MIN_TEMPERATURE;
-        } if (temperatures[edited] > MAX_TEMPERATURE) {
-            temperatures[edited] = MAX_TEMPERATURE;
+        if (_edited == NO_EDIT) return;
+        _temperatures[_edited] += val;
+        if (_temperatures[_edited] < MIN_TEMPERATURE) {
+            _temperatures[_edited] = MIN_TEMPERATURE;
+        } if (_temperatures[_edited] > MAX_TEMPERATURE) {
+            _temperatures[_edited] = MAX_TEMPERATURE;
         }
     }
 
