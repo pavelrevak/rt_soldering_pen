28071a3f47c70f0ffbf76e71594c52781955a472
Pavel Revak <pavel.revak@gmail.com>
2018-06-20 22:47:34 +0200

    removed unused fonts

--- src/lib/font.hpp
+++ src/lib/font.hpp
@@ -6,11 +6,11 @@ namespace lib {
 
 class Font {
 public:
-    static const uint8_t sans5[];
+    // static const uint8_t sans5[];
     static const uint8_t sans8[];
     static const uint8_t num7[];
     static const uint16_t num9[];
-    static const uint16_t num11[];
+    // static const uint16_t num11[];
     static const uint16_t num13[];
     static const uint32_t num22[];
 
