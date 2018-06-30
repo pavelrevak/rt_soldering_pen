c403f4ba9d250bb80442de80e5f7f016442fcc03
Pavel Revak <pavel.revak@gmail.com>
2018-06-30 21:29:35 +0200

    working versions are marked with '+' like v0.0.1+

--- src/meta.cpp
+++ src/meta.cpp
@@ -1,5 +1,5 @@
 #include "meta.hpp"
 
 const char *Meta::project = "RT soldering pen";
-const char *Meta::version = "v0.0.1";
+const char *Meta::version = "v0.0.1+";
 const char *Meta::build_date = __DATE__;
