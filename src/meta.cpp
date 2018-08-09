682fd598a016f19c306d693ee4b27f52c3450472
Pavel Revak <pavel.revak@gmail.com>
2018-08-09 19:55:33 +0200

    v0.2.0

--- src/meta.cpp
+++ src/meta.cpp
@@ -1,5 +1,5 @@
 #include "meta.hpp"
 
 const char *Meta::project = "RT soldering pen";
-const char *Meta::version = "v0.1.0+";
+const char *Meta::version = "v0.2.0";
 const char *Meta::build_date = __DATE__;
